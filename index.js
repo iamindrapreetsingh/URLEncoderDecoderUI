ab98c472b17f1365af17ccf950a8d2441ab363c6
Indrapreet Singh <indrapreet.singh@enquero.com>
2021-02-06 22:47:57 +0530

    Format Code

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ const output = document.querySelector(".textarea-output");
 const encodeURI = (input) => encodeURIComponent(input);
 const decodeURI = (input) => decodeURIComponent(input);
 
-btnEvent=(input)=>{
-
-    if(input===encodeBtn)
-    {
-    encodeBtn.style.backgroundColor="green";
-    decodeBtn.style.backgroundColor="thistle";
+btnEvent = (input) => {
+  if (input === encodeBtn) {
+    encodeBtn.style.backgroundColor = "green";
+    decodeBtn.style.backgroundColor = "thistle";
     outputMsg.innerHTML = "ENCODED URL:";
-    }
-
-    else
-    {
-    encodeBtn.style.backgroundColor="thistle";
-    decodeBtn.style.backgroundColor="green";
+  } else {
+    encodeBtn.style.backgroundColor = "thistle";
+    decodeBtn.style.backgroundColor = "green";
     outputMsg.innerHTML = "DECODED URL:";
-    }
-}
+  }
+};
 
-encodeBtn.addEventListener("click", ()=>btnEvent(encodeBtn));
-decodeBtn.addEventListener("click", ()=>btnEvent(decodeBtn));
-encodeBtn.addEventListener("click",()=>output.value=encodeURI(input.value));
-decodeBtn.addEventListener("click",()=>output.value=decodeURI(input.value));
+encodeBtn.addEventListener("click", () => btnEvent(encodeBtn));
+decodeBtn.addEventListener("click", () => btnEvent(decodeBtn));
+encodeBtn.addEventListener(
+  "click",
+  () => (output.value = encodeURI(input.value))
+);
+decodeBtn.addEventListener(
+  "click",
+  () => (output.value = decodeURI(input.value))
+);
